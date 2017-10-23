e7ca0a82692efca00688fa922b028d66c9b10be8
root <konkina.iana@gmail.com>
2017-10-23 01:18:44 -0700

    Comments added

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -1,12 +1,12 @@
 def generate_array_chunk():
-    """ """
+    """ Generate Y elements from user input of given length """
     line_str = input()
     chunk = [int(num) for num in line_str.strip().split()]
     return chunk
 
 
 def generate_multidimensional_array(size):
-    """ """
+    """ Generate Matrix with X*Y elements of given size """
     multidimensional_array = []
     for i in range(size):
         one_chunk = generate_array_chunk()
@@ -17,10 +17,16 @@ def generate_multidimensional_array(size):
 
 
 def pop_top(multidimensional_array):
+    """ Removes and returns the first item in the list
+        Complexity: O(k) (k represents the number passed in as an argument for pop)
+    """
     return multidimensional_array.pop(0)
 
 
 def pop_left(multidimensional_array):
+    """ Removes and returns last elements of each chunk
+        Complexity: O(n)*O(1) (pop last - O(1), iterate over chunk O(n))
+    """
     left_items = []
     for chunk in multidimensional_array:
         left_items.append(chunk.pop())
@@ -28,10 +34,15 @@ def pop_left(multidimensional_array):
 
 
 def pop_bottom(multidimensional_array):
+    """ """
     return reversed(multidimensional_array.pop())
 
 
 def pop_right(multidimensional_array):
+    """ Reverse remaining matrix, removes and returns first element from each chunk
+        Complexity: O(n) - to reverse * O(n) - to iterate * O(K) - to pop first element,
+        (k represents the number passed in as an argument for pop)
+    """
     right_items = []
     multidimensional_array = reversed(multidimensional_array)
     for chunk in multidimensional_array:
