cf55b43b9c5374cabd88cd93b7921c3d85d29e5d
root <konkina.iana@gmail.com>
2017-10-23 02:27:57 -0700

    formula Gaussa added

--- sum_nth_elements.py
+++ sum_nth_elements.py
@@ -1,18 +1,40 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
 
 
-#  O(n)
-# Проверяем остаток от деления - если 0, значит, суммируем.
+def sum_n_positive(n, number):
+    """
+    No iteration needed
+    formula: n(n+1)/2
+    https://brilliant.org/wiki/sum-of-n-n2-or-n3/#sum-of-first-n-positive-integers
+    https://gmatclub.com/forum/sum-of-n-positive-integers-formula-for-consecutive-integers-163301.html
+    """
+    how_many_n_items = number / n
+    return n * (how_many_n_items * (how_many_n_items + 1)/2)
+
+sum_each_3d = sum_n_positive(3, 1000)
+sum_each_5th = sum_n_positive(5, 1000)
+sum_intersections = sum_n_positive(3*5, 1000)
+print 'Formula only: %s' % str(sum_each_3d + sum_each_5th - sum_intersections)
+
+
+# iterate over list , sum only items which match (number % 3)
+# Complexity: O(n)
 sum_a = sum([number for number in range(1, 1000) if number % 3 == 0 or number % 5 == 0])
+print 'Iterate over list: %s' % sum_a
 
 
-# O = O(n / 3)
-# Берем каждый 3 елемент и складываем с каждым 5м елементом последовательности
 def func(n, m, arr):
+    """
+    Answer 2: Sum only each 3d and 5th element of arr
+    Complexity: O(n/3)
+    """
     left_step = n
     right_step = m
-    # Последовательность уникальных елементов: каждого 3го и 5го
+    # Unique items: each 3d and 5th
     items_to_sum = set()
     while n <= len(arr):
         items_to_sum.add(arr[n - 1])
@@ -24,7 +46,5 @@ def func(n, m, arr):
     return sum(items_to_sum)
 
 sum_b = func(3, 5, list(range(1, 1000)))
-
-
+print 'Iterate over each 3d: %s' % sum_b
 assert sum_a == sum_b, '{} is not equal {}'.format(sum_a, sum_b)
-print(sum_a, sum_b)
